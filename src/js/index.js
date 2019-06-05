68617c3c9a66901455b17ebe6d8d5a00d7dd62ea
e.kostandov <kostandov.e@gmail.com>
2019-06-06 01:09:31 +0300

    Almost done Search.js and searchView.js (with limit Recipes title, and appearing and disappering Loaders).

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,40 @@
-// Global app controller
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
 
-import num from './test';
+/* Global state of the app
+    - Search object
+    - Current recipe object
+    - Shopping list object
+    - Liked recipes
+*/
+const state = {};
 
-const x = 1212;
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = searchView.getInput();
 
-console.log(`I imported ${num} from another module! Variable x is ${x}.`);
+    if (query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+// import { get } from 'http';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = '360ee70f92c010d3df2b7725a09fe75d';
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+
+}
