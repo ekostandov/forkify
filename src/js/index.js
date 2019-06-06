2556a5248a84d7b4e9063edc707edddf187f3e48
e.kostandov <kostandov.e@gmail.com>
2019-06-06 20:24:35 +0300

    Implemeted search relults pagination.

--- src/js/index.js
+++ src/js/index.js
@@ -37,4 +37,14 @@ elements.searchForm.addEventListener('submit', e => {
     controlSearch();
 });
 
-search.getResults();
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+        console.log(goToPage);
+    }
+});
+
+//Search.getResults();
